eec920032ff43fd1693bc07c2f70b90707ba2e45
jamiegiuliano <jamiepgiuliano@gmail.com>
2018-02-06 19:05:28 +0000

    Refactored magic numbers in cli.

--- lib/most_haunted/cli.rb
+++ lib/most_haunted/cli.rb
@@ -76,7 +76,7 @@ class MostHauntedCli::CLI
             DOC
             input= gets.strip.downcase
             
-            if input.to_i > 0 && input.to_i < 53
+            if input.to_i > 0 && input.to_i <= MostHauntedCli::States.haunted.length
                 MostHauntedCli::States.open_state_info(input.to_i)
             elsif input == 'main menu'
                 start
@@ -107,7 +107,7 @@ class MostHauntedCli::CLI
             DOC
             input = gets.strip.downcase
                 
-            if input.to_i > 0 && input.to_i < 11
+            if input.to_i > 0 && input.to_i <= MostHauntedCli::America.haunted.size
                 puts ''
                 MostHauntedCli::America.america_descriptions(MostHauntedCli::America.indexes[input.to_i-1])
             elsif input == "list"
